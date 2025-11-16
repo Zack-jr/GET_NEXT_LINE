ded3bc80c480a02a294c29601b210900489c485b
Zack <girardzack04@example.com>
2025-11-16 21:09:42 +0100

    save changes

--- get_next_line.c
+++ get_next_line.c
@@ -16,7 +16,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 // strncat to concatenate stash + next buffer
 
 // return line from a file using file descriptor
@@ -55,15 +54,15 @@ ssize_t find_newline(char *str)
     return (-1);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str;
-	int		total_len;
 	size_t	i;
+    char *old_str;
 
 	i = 0;
-	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = malloc(sizeof(char) * total_len);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+    old_str = s1;
 	if (!str)
 		return (NULL);
 	while (*s1 != '\0')
@@ -79,9 +78,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		i++;
 	}
 	str[i] = '\0';
+    free(old_str);
 	return (str);
 }
-
 char	*ft_strdup(const char *s)
 {
 	char	*dup;
@@ -102,46 +101,61 @@ char	*ft_strdup(const char *s)
 	return (dup);
 }
 
-char     *get_next_line(int fd)
+char *process_line(char **stash, int fd, char *buffer, int pos, int flag)
 {
-    char buffer[BUFFER_SIZE];
-    static char *stash;
     char *line;
-    int i;
-    ssize_t bytes_read;
     char *new_stash;
 
-    //if (fd == 0 || BUFFER_SIZE == 0)
-      //  return (NULL);
+    if (flag = 1)
+    {
+        line = malloc((sizeof(char) * pos ) + 1);
+        if (!line)
+            return (NULL);
+        line = strncpy(line, stash[fd], pos);
+        line[pos] = '\0';
+        new_stash = ft_strdup(stash[fd] + pos);
+        free(stash[fd]);
+        stash[fd] = new_stash;
+        free(buffer);
+        return (line);
+    }
 
+}
+
+char     *get_next_line(int fd)
+{
+    char *buffer;
+    static char *stash[1025];
+    char *line;
+    int pos;
+    ssize_t bytes_read;
+
+    if ((fd >= 1 && fd <= 2) || fd < 0 || fd >= 1024 || BUFFER_SIZE <= 0)
+        return (NULL);
+    buffer = malloc(BUFFER_SIZE + 1);
+    if (!buffer)
+        return (NULL);
     bytes_read = read(fd, buffer, BUFFER_SIZE);
     while (bytes_read > 0)
     {
         buffer[bytes_read] = '\0';
-        if (stash == NULL)
+        if (stash[fd] == NULL)
         {
-            stash = malloc(bytes_read + 1);
-            stash[0] = '\0';
+            stash[fd] = malloc(bytes_read + 1);
+            stash[fd][0] = '\0';
         }
-        stash = ft_strjoin(stash, buffer);
-        i = find_newline(stash); // donner position de la newline
-        if (i > 0) // si newline
-        {
-            line = malloc((sizeof(char) * i ) + 1);
-            line = strncpy(line, stash, i);
-            line[i] = '\0';
-            new_stash = ft_strdup(stash + i);
-            free(stash);
-            stash = new_stash;
-            return (line);
-        }
+        stash[fd] = ft_strjoin(stash[fd], buffer);
+        pos = find_newline(stash[fd]); // donner position de la newline
+        if (pos > 0) // si newline
+            return (process_line(stash, fd, buffer, pos, 1));
         bytes_read = read(fd, buffer, BUFFER_SIZE);
     }
-    if (stash && *stash)
+    if (stash[fd] && *stash[fd])
     {
-        line = ft_strdup(stash);
-        free(stash);
-        stash = NULL;
+        line = ft_strdup(stash[fd]);
+        free(stash[fd]);
+        stash[fd] = NULL;
+        free(buffer);
         return (line);
     }
     return NULL;
@@ -151,13 +165,25 @@ int main(void)
 {
     int fd = open("textinput.txt", O_RDONLY);
     //printf("%li", find_newline("abdd\ne"));
-    
-    char *line = get_next_line(fd);
-    while (line != NULL)
+   //int fd2 = open("lyrics.txt", O_RDONLY);
+    char *line1;
+    //char *line2;
+    dup2(fd, 5);
+     line1 = get_next_line(5);
+     printf("%s", line1);
+     free(line1);
+
+    //line2 = get_next_line(fd2);
+   /* while (line1 && line2)
     {
-        printf("%s", line);
-        line = get_next_line(fd);
-    }
+        printf("%s", line1);
+         free(line1);
+        printf("%s", line2);
+         free(line2);
+        line1 = get_next_line(fd);
+        line2 = get_next_line(fd2);
+    }*/
+
     close(fd);
 
 }
